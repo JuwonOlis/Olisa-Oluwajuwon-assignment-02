fd5409efa703cfee97324aba2132d8173b9a50f3
Olisa Oluwajuwon <103125813+cantylesar@users.noreply.github.com>
2022-07-14 01:38:06 +0000

    challenge 1 and challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -30,10 +30,15 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(arrayOfNumbers) { let evenNumbers = 0;
+for(let i=0; i < arrayOfNumbers.length; i++) {
+if(arrayOfNumbers[i]%2 === 0){
+evenNumbers++
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+}
+    return evenNumbers;
+}
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
