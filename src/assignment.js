d5c3472af16ee36d36b8772ce9c020b376cbd868
Olisa Oluwajuwon <103125813+cantylesar@users.noreply.github.com>
2022-07-14 01:11:04 +0000

    challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,13 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers) { let sum = 0;
+for(let i of arrayOfNumbers) {
+  sum += i
+} 
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
